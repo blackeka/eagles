8eabe99c55abce101b86b23890a3099c6fdec846
ANR <anr@Adityas-MacBook-Pro.local>
2017-10-05 09:56:12 -0400

    student and teacher dashboards working with join class

--- frontend/components/Subcomponents/SingleClass.js
+++ frontend/components/Subcomponents/SingleClass.js
@@ -8,12 +8,13 @@ const SingleClass = (props) => {
       <p> Lessons: {props.selectedClass.lessons} </p>
       <p> Students: </p>
       <div classID='studentsenrolled'>
-        {props.selectedClass.students ? props.selectedClass.students.forEach((student) => (
-          <div>
-            <p> text from yes part of for each </p>
-            <p> {student.username} </p>
+        <ol>
+        {props.selectedClass.students ? props.selectedClass.students.map((student, i) => (
+          <div key={i}>
+            <li> {student.username} </li>
           </div>
         )) : ''}
+        </ol>
       </div>
     </div>
   )
