1fac73816a522ace76703923e32c30c121fbd6f5
ANRanganathan <anranga95@gmail.com>
2017-10-05 19:07:53 -0400

    deleted notification center last time

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -18,8 +18,6 @@ class App extends Component {
           userRole = {this.props.userRole}
           isLoggedIn = {this.props.isLoggedIn}
         />
-      <Notifications />
-        { this.props.children || 'no children!' }
       </div>
     );
   }
